064dd16b55732cf47155c8148bbab1f149c81a67
fultoncarolyn <carolyn.fulton@colorado.edu>
2024-12-12 17:53:59 -0700

    update for mtg dec 13

--- sims.R
+++ sims.R
@@ -1,26 +1,29 @@
 # Simulation of Forward Generation Interval Measurement
 
-#GOAL: Create df with original timing of infection, timing of secondary infection, 
+# GOAL: Create df with original timing of infection, timing of secondary infection, 
 #       and the different between for one generation/layer
 
-#Step One: Generate a number of infected individuals and the times at which they were infected
-#           Refer to this as the initial cohort
+# Loading packages
+library(ggplot2)
 
 # Parameters (for now)
 beta = 1.75
 gamma = 0.35
 
+###### Step One: Generate a number of infected individuals and the times at which they were infected ###### 
+
+# Refer to this as the initial cohort
+
 initcohort = c()
-initcohort <- sort(c(0.2,0.1)) #define strict first individuals and their timing
+initcohort <- sort(c(0.2,0.1,0.3,0.4,0.5,0.6,0.7,0.8)) # for now define strict first individuals and their timing (diff vs same)
 
-#Step Two: Order the initial cohort chronologically
+####### Step Two: Order the initial cohort chronologically (if necessary) ###### 
 
-###initcohort = sort(init)
-
-#Step Three: Loop over initial cohort to generate secondary infections
+####### Step Three: Loop over initial cohort to generate secondary infections ######
+#######             AND map to data frame for manipulation ###### 
 
 #Assumptions: All infections stick -> contact implies infected
-#             The timing of infection for secondary is identical
+#             The timing of infection for secondary is NOT identical
 
 
 funloop <- function(vector){
@@ -36,13 +39,13 @@ for (i in 1:length(vector)){
   contacts = rpois(1,beta/gamma) # generate number of secondary contacts
   print(paste0("Number of contacts:", contacts,"for individual:", i," from initial cohort."))
   if (contacts == 0){
-    #do something here for df
-    
+    #do something here for df or is next fine?
+    next
   } else {
     for (j in 1:contacts){
-      timeofinfect = rexp(1,gamma) #timing of secondary infection
-      print(paste0("Timing of secondary infection:", timeofinfect,"for contact:", j," from initial cohort member", i, "."))
-      output = c(i,vector[i],timeofinfect,timeofinfect - vector[i]) #gen int timing vs timeofinfect
+      infecttime = rexp(1,gamma) #timing of secondary infection
+      print(paste0("Timing of secondary infection:", infecttime,"for contact:", j," from initial cohort member", i, "."))
+      output = c(i,vector[i],vector[i] + infecttime,infecttime) 
       contactdata <- rbind(contactdata, output)
     }
   }
@@ -50,7 +53,17 @@ for (i in 1:length(vector)){
 names(contactdata) <- c('personnumber','initcohorttime', 'secondinfecttime','genint') # names aren't working but not worried about right now...
 
 print(contactdata)
+
+###### Step Four: Generate plots #####
+
+#Forward Scheme
+ggplot(contactdata, aes(initcohorttime, genint)) + geom_point() + labs(x = 'Time of Initial Cohort Infection', y = 'Generation Interval', title = 'Forward Model Schematic')
+
+#Density/Distribution
+ggplot(contactdata, aes(x = genint)) + geom_histogram(bins = 25) + labs(x = 'Generation Interval', y = 'Number of Secondary', title = 'Forward Model Histogram')
+
+
 }
 
-#Step Four: Map these to a data frame for easier manipulation
+
 
